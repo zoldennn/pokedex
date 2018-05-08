ab004f714d2e5e8336ab6a043cfdd766444940db
camilo.spinelli <camilo.spinelli@globallogic.com>
2018-05-08 15:26:25 -0300

    added splash screen

--- app/src/main/java/com/example/camilo/pokedex/acts/PokemonListActivity.java
+++ app/src/main/java/com/example/camilo/pokedex/acts/PokemonListActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Window;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -22,6 +23,7 @@ import com.example.camilo.pokedex.services.PokemonService;
 import com.example.camilo.pokedex.utils.PokemonListFetcher;
 import com.example.camilo.pokedex.utils.Utils;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -42,6 +44,7 @@ public class PokemonListActivity extends AppCompatActivity implements PokemonSer
     private PokemonListFetcher mPokemonListFetcher;
     private LoadingDialog mLoadingDialog;
     private Toolbar mToolbar;
+    private List<Pokemon> lista = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,17 +57,23 @@ public class PokemonListActivity extends AppCompatActivity implements PokemonSer
         getSupportActionBar().setIcon(R.drawable.pokelogo1);
         setupRecyclerView();
 
-        showLoadingDialog();
+        Intent intent = getIntent();
+        Bundle bundle = intent.getBundleExtra("bundle");
+        lista = (List<Pokemon>) bundle.getSerializable("lista");
+        if(lista!=null){
+            mPokemonListAdapter.addNewPokemonList(lista);
+        }
+        Log.d("TAG", lista.get(2).getName());
+        //showLoadingDialog();
 
         mMustCharge = true; // This var avoids to make api calls continuously
-        mOffset = 0; // To call pokemon from 0 to 20
-        mPokemonListFetcher.callPokemonApi(); // Make API call
+        //mOffset = 0; // To call pokemon from 0 to 20
+        //mPokemonListFetcher.callPokemonApi(); // Make API call
     }
 
     @Override
     public void renderPokemonList(List<Pokemon> pokemonList) {
         mPokemonListAdapter.addNewPokemonList(pokemonList);
-        //mLoadingDialog.dismiss();
     }
 
     @Override
---
+++ app/src/main/java/com/example/camilo/pokedex/acts/SplashScreen.java
@@ -0,0 +1,57 @@
+package com.example.camilo.pokedex.acts;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import com.example.camilo.pokedex.R;
+import com.example.camilo.pokedex.models.Pokemon;
+import com.example.camilo.pokedex.services.PokemonService;
+import com.example.camilo.pokedex.utils.PokemonListFetcher;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SplashScreen extends AppCompatActivity implements PokemonService {
+
+    private PokemonListFetcher mPokemonListFetcher;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // To fullscreen
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash_screen);
+
+        mPokemonListFetcher = new PokemonListFetcher(this, 0, this); // Instantiate this class to make the calls
+        mPokemonListFetcher.callPokemonApi(); // Make API call
+    }
+
+    @Override
+    public void renderPokemonList(List<Pokemon> pokemonList) {
+        Intent intent = new Intent(this, PokemonListActivity.class);
+        Bundle extras = new Bundle();
+        extras.putSerializable("lista", (Serializable) pokemonList);
+        intent.putExtra("bundle", extras);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onPokemonItemClick(Pokemon pokemon, int pos, ImageView img) {
+        // Do nothing here
+    }
+
+    @Override
+    public void renderPokemon(Pokemon pokemon) {
+        // Do nothing here
+    }
+}
